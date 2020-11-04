30083156286fa7c0f0d0613036fe07c98230b595
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-11-04 17:10:53 +0100

    wip

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// creare l’header come da screeshot allegato.
+// Ogni voce di menù avrà un suo dropdown menù che viene fuori al click.
+
+$(document).ready(function(){
+
+    $('.nav-right > ul').click(function () {
+        $('ul > li').toggleClass('show');
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
